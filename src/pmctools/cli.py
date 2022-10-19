ccf6d97325f43d1aa850727ad798bd514f2a9e95
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 09:09:02 +0000

    Adding tarfile member sanitization to extractall()

--- src/pmctools/cli.py
+++ src/pmctools/cli.py
@@ -65,7 +65,26 @@ def uncompress_cli(input_dir: str, output_dir: str):
         tar_filepath = os.path.join(input_dir, filename)
 
         with tarfile.open(tar_filepath) as input_file:
-            input_file.extractall(path=output_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(input_file, path=output_dir)
 
 
 @cli.command("REGROUP")
